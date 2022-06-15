62e1c00e3a15797da25507df2a2deab193338c7e
Rimantas <belauzas@yahoo.com>
2022-06-15 20:16:45 +0300

    Page router komentarai

--- js/class-router.js
+++ js/class-router.js
@@ -12,21 +12,29 @@ class PageHome {
     }
 }
 
+// susiejimas ka vartotojas gali parasyti
+// ir ka mes tada darytume (kuria klase kviesti)
 const router = {
     '': PageHome,
     '404': Page404,
 }
 
-const trimmedPath = 'as';
+// vartotojo ivestas kelias
+const trimmedPath = '';
 
+// reikiamos naudoti klases atranka
+// kai 404 yra musu default pasirinkimas
 let pageClass = router['404'];
 if (trimmedPath in router) {
     pageClass = router[trimmedPath];
 }
 
+// pagal rasta naudojimui klase yra kuriamas jos objektas
 const pageObj = new pageClass();
+// ir tas objektas generuoja savo HTML turini
 const HTML = pageObj.render();
 
+// spausdinam...
 console.log(pageClass);
 console.log(pageObj);
 console.log(HTML);
